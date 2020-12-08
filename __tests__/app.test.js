1b79e0e9bfa0975d62d70e069b324627cd7f4834
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-08 18:58:11 -0500

    complete remaining tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -33,5 +33,76 @@ describe('App routes for Friend model', () => {
       hiddenTalent: 'being awesome'
     });
   });
+
+  it('Use GET route to view all friends', async () => {
     
+    const data = await request(app)
+      .get('/api/v1/friends');
+
+    expect(data.body).toEqual([{
+      id: '1',
+      name: 'Isaac',
+      age: 41,
+      isBestFriend: true,
+      hiddenTalent: 'being awesome'
+    }]);
+  });
+    
+  it('Use GET route to view one friend', async () => {
+    
+    const data = await request(app)
+      .get('/api/v1/friends');
+
+    expect(data.body).toEqual([{
+      id: '1',
+      name: 'Isaac',
+      age: 41,
+      isBestFriend: true,
+      hiddenTalent: 'being awesome'
+    }]);
+  });
+    
+  it('Use PUT route to update Friend', async () => {
+    
+    const friend = await Friend.insert({ name: 'Isaac', age: '41', isBestFriend: 'true', hiddenTalent: 'being REALLY awesome'
+    });
+
+    const data = await request(app)
+      .put(`/api/v1/friends/${friend.id}`)
+      .send({
+        name: 'Isaac',
+        age: 41,
+        isBestFriend: true,
+        hiddenTalent: 'being REALLY awesome'
+      });
+
+    expect(data.body).toEqual({
+      ...friend,
+      name: 'Isaac',
+      age: 41,
+      isBestFriend: true,
+      hiddenTalent: 'being REALLY awesome'
+    });
+  });
+
+  it('Use DELETE route to delete Friend', async () => {
+    
+    const data = await request(app)
+      .delete('/api/v1/friends/1')
+      .send({
+        name: 'Isaac',
+        age: 41,
+        isBestFriend: true,
+        hiddenTalent: 'being awesome'
+      });
+
+    expect(data.body).toEqual({
+      id: '1',
+      name: 'Isaac',
+      age: 41,
+      isBestFriend: true,
+      hiddenTalent: 'being awesome'
+    });
+  });
+
 });
